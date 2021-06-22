6cd3b923594451f94d9a1c3aa73217b082f324b2
Sadid369 <sadid.jones@gmail.com>
2021-06-22 08:33:29 +0600

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
               ),
         ),
       ),
-      title: 'Personal Expenses 2',
+      title: 'Personal Expenses 2-',
       home: MyHomePage(),
     );
   }
